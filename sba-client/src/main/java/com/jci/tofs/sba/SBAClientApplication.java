9291550509f82d936c4f39f329488ddf7ef063d1
himansunayak <hnayak@tycoint.com>
2018-01-15 12:17:33 +0000

    eureka added

--- sba-client/src/main/java/com/jci/tofs/sba/SBAClientApplication.java
+++ sba-client/src/main/java/com/jci/tofs/sba/SBAClientApplication.java
@@ -3,10 +3,11 @@ package com.jci.tofs.sba;
 import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan
 @EnableScheduling
